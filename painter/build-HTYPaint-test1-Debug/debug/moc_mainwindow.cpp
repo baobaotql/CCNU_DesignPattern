5fcba2020c749aff30dab25c612dbd6ac2e05ea7
baobao <baobaotql@163.com>
2020-05-11 21:07:00 +0800

    窗口工具栏调整

---
+++ painter/build-HTYPaint-test1-Debug/debug/moc_mainwindow.cpp
@@ -0,0 +1,311 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../baobao_Painter/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.9.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[45];
+    char stringdata0[996];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 24), // "on_action_open_triggered"
+QT_MOC_LITERAL(2, 36, 0), // ""
+QT_MOC_LITERAL(3, 37, 24), // "on_action_save_triggered"
+QT_MOC_LITERAL(4, 62, 26), // "on_action_saveas_triggered"
+QT_MOC_LITERAL(5, 89, 26), // "on_action_import_triggered"
+QT_MOC_LITERAL(6, 116, 29), // "on_action_changelog_triggered"
+QT_MOC_LITERAL(7, 146, 27), // "on_action_aboutQt_triggered"
+QT_MOC_LITERAL(8, 174, 25), // "on_action_about_triggered"
+QT_MOC_LITERAL(9, 200, 24), // "on_action_undo_triggered"
+QT_MOC_LITERAL(10, 225, 24), // "on_action_redo_triggered"
+QT_MOC_LITERAL(11, 250, 24), // "on_action_copy_triggered"
+QT_MOC_LITERAL(12, 275, 25), // "on_action_paste_triggered"
+QT_MOC_LITERAL(13, 301, 32), // "on_action_setWallpaper_triggered"
+QT_MOC_LITERAL(14, 334, 28), // "on_action_property_triggered"
+QT_MOC_LITERAL(15, 363, 24), // "on_action_help_triggered"
+QT_MOC_LITERAL(16, 388, 26), // "on_action_excude_triggered"
+QT_MOC_LITERAL(17, 415, 24), // "on_action_gray_triggered"
+QT_MOC_LITERAL(18, 440, 26), // "on_action_invert_triggered"
+QT_MOC_LITERAL(19, 467, 24), // "on_action_blur_triggered"
+QT_MOC_LITERAL(20, 492, 26), // "on_action_mosaic_triggered"
+QT_MOC_LITERAL(21, 519, 31), // "on_action_transparent_triggered"
+QT_MOC_LITERAL(22, 551, 29), // "on_action_adjustRGB_triggered"
+QT_MOC_LITERAL(23, 581, 28), // "on_action_clipPath_triggered"
+QT_MOC_LITERAL(24, 610, 29), // "on_actionRotateLeft_triggered"
+QT_MOC_LITERAL(25, 640, 30), // "on_actionRotateRight_triggered"
+QT_MOC_LITERAL(26, 671, 35), // "on_actionMirrorHorizontal_tri..."
+QT_MOC_LITERAL(27, 707, 33), // "on_actionMirrorVertical_trigg..."
+QT_MOC_LITERAL(28, 741, 23), // "on_actionFont_triggered"
+QT_MOC_LITERAL(29, 765, 12), // "setColorFill"
+QT_MOC_LITERAL(30, 778, 14), // "setColorBorder"
+QT_MOC_LITERAL(31, 793, 15), // "spinValueChange"
+QT_MOC_LITERAL(32, 809, 10), // "textChange"
+QT_MOC_LITERAL(33, 820, 14), // "onSpinwChanged"
+QT_MOC_LITERAL(34, 835, 14), // "onSpinhChanged"
+QT_MOC_LITERAL(35, 850, 15), // "onSpinwrChanged"
+QT_MOC_LITERAL(36, 866, 15), // "onSpinhrChanged"
+QT_MOC_LITERAL(37, 882, 28), // "checkBox_color_borderChanged"
+QT_MOC_LITERAL(38, 911, 26), // "checkBox_color_fillChanged"
+QT_MOC_LITERAL(39, 938, 4), // "open"
+QT_MOC_LITERAL(40, 943, 9), // "setPicker"
+QT_MOC_LITERAL(41, 953, 5), // "color"
+QT_MOC_LITERAL(42, 959, 11), // "addPenWidth"
+QT_MOC_LITERAL(43, 971, 14), // "reducePenWidth"
+QT_MOC_LITERAL(44, 986, 9) // "cutSelect"
+
+    },
+    "MainWindow\0on_action_open_triggered\0"
+    "\0on_action_save_triggered\0"
+    "on_action_saveas_triggered\0"
+    "on_action_import_triggered\0"
+    "on_action_changelog_triggered\0"
+    "on_action_aboutQt_triggered\0"
+    "on_action_about_triggered\0"
+    "on_action_undo_triggered\0"
+    "on_action_redo_triggered\0"
+    "on_action_copy_triggered\0"
+    "on_action_paste_triggered\0"
+    "on_action_setWallpaper_triggered\0"
+    "on_action_property_triggered\0"
+    "on_action_help_triggered\0"
+    "on_action_excude_triggered\0"
+    "on_action_gray_triggered\0"
+    "on_action_invert_triggered\0"
+    "on_action_blur_triggered\0"
+    "on_action_mosaic_triggered\0"
+    "on_action_transparent_triggered\0"
+    "on_action_adjustRGB_triggered\0"
+    "on_action_clipPath_triggered\0"
+    "on_actionRotateLeft_triggered\0"
+    "on_actionRotateRight_triggered\0"
+    "on_actionMirrorHorizontal_triggered\0"
+    "on_actionMirrorVertical_triggered\0"
+    "on_actionFont_triggered\0setColorFill\0"
+    "setColorBorder\0spinValueChange\0"
+    "textChange\0onSpinwChanged\0onSpinhChanged\0"
+    "onSpinwrChanged\0onSpinhrChanged\0"
+    "checkBox_color_borderChanged\0"
+    "checkBox_color_fillChanged\0open\0"
+    "setPicker\0color\0addPenWidth\0reducePenWidth\0"
+    "cutSelect"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      42,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  224,    2, 0x08 /* Private */,
+       3,    0,  225,    2, 0x08 /* Private */,
+       4,    0,  226,    2, 0x08 /* Private */,
+       5,    0,  227,    2, 0x08 /* Private */,
+       6,    0,  228,    2, 0x08 /* Private */,
+       7,    0,  229,    2, 0x08 /* Private */,
+       8,    0,  230,    2, 0x08 /* Private */,
+       9,    0,  231,    2, 0x08 /* Private */,
+      10,    0,  232,    2, 0x08 /* Private */,
+      11,    0,  233,    2, 0x08 /* Private */,
+      12,    0,  234,    2, 0x08 /* Private */,
+      13,    0,  235,    2, 0x08 /* Private */,
+      14,    0,  236,    2, 0x08 /* Private */,
+      15,    0,  237,    2, 0x08 /* Private */,
+      16,    0,  238,    2, 0x08 /* Private */,
+      17,    0,  239,    2, 0x08 /* Private */,
+      18,    0,  240,    2, 0x08 /* Private */,
+      19,    0,  241,    2, 0x08 /* Private */,
+      20,    0,  242,    2, 0x08 /* Private */,
+      21,    0,  243,    2, 0x08 /* Private */,
+      22,    0,  244,    2, 0x08 /* Private */,
+      23,    0,  245,    2, 0x08 /* Private */,
+      24,    0,  246,    2, 0x08 /* Private */,
+      25,    0,  247,    2, 0x08 /* Private */,
+      26,    0,  248,    2, 0x08 /* Private */,
+      27,    0,  249,    2, 0x08 /* Private */,
+      28,    0,  250,    2, 0x08 /* Private */,
+      29,    0,  251,    2, 0x08 /* Private */,
+      30,    0,  252,    2, 0x08 /* Private */,
+      31,    1,  253,    2, 0x08 /* Private */,
+      32,    1,  256,    2, 0x08 /* Private */,
+      33,    1,  259,    2, 0x08 /* Private */,
+      34,    1,  262,    2, 0x08 /* Private */,
+      35,    1,  265,    2, 0x08 /* Private */,
+      36,    1,  268,    2, 0x08 /* Private */,
+      37,    1,  271,    2, 0x08 /* Private */,
+      38,    1,  274,    2, 0x08 /* Private */,
+      39,    1,  277,    2, 0x08 /* Private */,
+      40,    1,  280,    2, 0x08 /* Private */,
+      42,    0,  283,    2, 0x08 /* Private */,
+      43,    0,  284,    2, 0x08 /* Private */,
+      44,    0,  285,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void, QMetaType::QColor,   41,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_action_open_triggered(); break;
+        case 1: _t->on_action_save_triggered(); break;
+        case 2: _t->on_action_saveas_triggered(); break;
+        case 3: _t->on_action_import_triggered(); break;
+        case 4: _t->on_action_changelog_triggered(); break;
+        case 5: _t->on_action_aboutQt_triggered(); break;
+        case 6: _t->on_action_about_triggered(); break;
+        case 7: _t->on_action_undo_triggered(); break;
+        case 8: _t->on_action_redo_triggered(); break;
+        case 9: _t->on_action_copy_triggered(); break;
+        case 10: _t->on_action_paste_triggered(); break;
+        case 11: _t->on_action_setWallpaper_triggered(); break;
+        case 12: _t->on_action_property_triggered(); break;
+        case 13: _t->on_action_help_triggered(); break;
+        case 14: _t->on_action_excude_triggered(); break;
+        case 15: _t->on_action_gray_triggered(); break;
+        case 16: _t->on_action_invert_triggered(); break;
+        case 17: _t->on_action_blur_triggered(); break;
+        case 18: _t->on_action_mosaic_triggered(); break;
+        case 19: _t->on_action_transparent_triggered(); break;
+        case 20: _t->on_action_adjustRGB_triggered(); break;
+        case 21: _t->on_action_clipPath_triggered(); break;
+        case 22: _t->on_actionRotateLeft_triggered(); break;
+        case 23: _t->on_actionRotateRight_triggered(); break;
+        case 24: _t->on_actionMirrorHorizontal_triggered(); break;
+        case 25: _t->on_actionMirrorVertical_triggered(); break;
+        case 26: _t->on_actionFont_triggered(); break;
+        case 27: _t->setColorFill(); break;
+        case 28: _t->setColorBorder(); break;
+        case 29: _t->spinValueChange((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 30: _t->textChange((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 31: _t->onSpinwChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 32: _t->onSpinhChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 33: _t->onSpinwrChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 34: _t->onSpinhrChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 35: _t->checkBox_color_borderChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 36: _t->checkBox_color_fillChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 37: _t->open((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 38: _t->setPicker((*reinterpret_cast< QColor(*)>(_a[1]))); break;
+        case 39: _t->addPenWidth(); break;
+        case 40: _t->reducePenWidth(); break;
+        case 41: _t->cutSelect(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(const_cast< MainWindow*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 42)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 42;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 42)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 42;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
